a009459190237bc8888d07c164f8aab471daa0f3
Shibhani <140485226+Shibhani-V@users.noreply.github.com>
2025-04-03 17:24:55 +0530

    changes

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+let images = ["image1.png", "image2.png", "image3.png"];
+let index = 0;
 // Change Image Background Color
 function changeBackground() {
-	let imageContainer = document.querySelector(".image-container");
-	let newColor = imageContainer.style.backgroundColor === "yellow" ? "white" : "yellow";
-	imageContainer.style.backgroundColor = newColor;
+	let testImage = document.getElementById("testImage");
+	let selectedColor = document.getElementById("colorPicker").value;
+
+	// Define image mapping for each selected color
+	let imageMap = {
+			"white": "image1.png",
+			"deep_pink": "image_deep_pink.png",
+			"pink": "image_pastel_pink.png",
+			"green": "image_green.png"
+	};
+
+	// Update the image source based on selection
+	testImage.src = imageMap[selectedColor];
 }
 
 // Carousel Functionality
-let images = ["image1.png", "image2.png", "image3.png"];
-let index = 0;
-
 function nextImage() {
 	index = (index + 1) % images.length;
 	document.getElementById("carouselImage").src = images[index];
